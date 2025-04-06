412597422fa72c15e13de77844e9979803f9c6f1
fatmabostan <fatma_bostan_@outlook.com>
2025-04-06 20:12:41 +0300

    feat: add auth-protected layout with server-side redirect for unauthenticated users

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,20 @@
+export const dynamic = 'force-dynamic'; //This page must be recreated on each user request. Because user-specific data (auth/session) is required
+import { onAuthenticateUser } from '@/actions/user'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = { children: React.ReactNode }
+
+const Layout = async ({ children }: Props) => {
+    const auth = await onAuthenticateUser();
+    if(!auth.user){
+        redirect('/sign-in');
+    }
+  return (
+    <div className='w-full min-h-screen'>
+        {children}
+    </div>
+  )
+}
+
+export default Layout
