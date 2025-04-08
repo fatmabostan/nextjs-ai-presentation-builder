7247c3e1a1a6e179829fd28c3bdf3444960740e3
fatmabostan <fatma_bostan_@outlook.com>
2025-04-08 23:41:34 +0300

    feat(project): add getRecentProjects with auth check and error handling

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -1,30 +1,58 @@
 import { onAuthenticateUser } from "./user";
 import { client } from "@/lib/prisma";
 
-export const getAllProjects = async() => {
-  try{
+export const getAllProjects = async () => {
+  try {
     const checkUser = await onAuthenticateUser();
-    if(checkUser.status !== 200 || !checkUser.status){
-      return { status:403, error: 'User Not Authenticated' };
+    if (checkUser.status !== 200 || !checkUser.status) {
+      return { status: 403, error: 'User Not Authenticated' };
     }
 
     const projects = await client.project.findMany({
-      where:{
+      where: {
         userId: checkUser.user?.id,
         isDeleted: false,
       },
-      orderBy:{
+      orderBy: {
         updatedAt: 'desc',
       }
     });
 
-    if(projects.length === 0){
-      return { status:404, error: 'No Project Found' };
+    if (projects.length === 0) {
+      return { status: 404, error: 'No Project Found' };
     }
 
     return { status: 200, data: projects };
-  }catch(error){
+  } catch (error) {
     console.error("ERROR", error)
     return { status: 500, error: 'Internal Server Error' }
   }
+}
+
+export const getRecentProjects = async () => {
+  try {
+    const checkUser = await onAuthenticateUser();
+    if (checkUser.status !== 200 || !checkUser.status) {
+      return { status: 403, error: 'User Not Authenticated' };
+    }
+    const projects = await client.project.findMany({
+      where: {
+        userId: checkUser.user?.id,
+        isDeleted: false
+      },
+      orderBy: {
+        updatedAt: 'desc'
+      },
+      take: 8,
+    })
+    if (projects.length === 0) {
+      return { status: 404, error: "No recent projects available." }
+    }
+    return { status: 200, data: projects }
+    
+  } catch (error) {
+    console.error("ERROR", error)
+    return { status: 500, error: 'Internal Server Error' }
+  }
+
 }
