cb3b81b1314f532e6339ccbc05fe3839b495116e
fatmabostan <fatma_bostan_@outlook.com>
2025-04-08 23:38:38 +0300

    feat(dashboard): add auth-protected layout with sidebar integration

---
+++ src/app/(protected)/(pages)/layout.tsx
@@ -0,0 +1,23 @@
+import { getRecentProjects } from '@/actions/project';
+import { onAuthenticateUser } from '@/actions/user';
+import AppSidebar from '@/components/global/app-sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+type Props = { children: React.ReactNode }
+
+const Layout = async ( {children} : Props) => {
+    const recentProjects = await getRecentProjects();
+    const checkUser = await onAuthenticateUser();
+    if(!checkUser.user){
+        redirect('/sign-in');
+    }
+  return (
+    <SidebarProvider>
+        <AppSidebar user={checkUser.user} recentProjects={recentProjects.data || []} />
+    </SidebarProvider>
+  )
+}
+
+export default Layout
