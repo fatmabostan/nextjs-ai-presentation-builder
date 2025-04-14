e56c905c6572ef26d011026d034e2c12be45c384
fatmabostan <fatma_bostan_@outlook.com>
2025-04-15 00:12:15 +0300

    feat(sidebar): redesign layout and add user footer

--- src/components/global/app-sidebar/index.tsx
+++ src/components/global/app-sidebar/index.tsx
@@ -3,7 +3,6 @@ import { AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarGroup,
   SidebarHeader,
   SidebarMenuButton,
  } from '@/components/ui/sidebar'
@@ -13,6 +12,7 @@ import React from 'react'
 import NavMain from './nav-main'
 import { data } from '@/lib/constants'
 import RecentOpen from './recent-open.'
+import NavFooter from './nav-footer'
 
 type Props = {
   recentProjects: Project[],
@@ -23,27 +23,29 @@ type Props = {
 const AppSidebar = ( {recentProjects, user, ...props}: Props) => {
   return (
     <Sidebar collapsible='icon' 
-      className='max-w-[212px] bg-background-90'
+      className='max-w-[220px] bg-background-90'
       {...props}>
-      <SidebarHeader className='pt-6 px-3 pb-0'>
+      <SidebarHeader className='pt-6 px-2 pb-0'>
         <SidebarMenuButton size={'lg'}
         className='data-[state=open]:text-slidebar-accent-foreground'>
           <div className='flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground'>
-              <Avatar className='h-9 w-9 rounded-full'>
+              <Avatar className='w-9 rounded-full'>
              <AvatarImage src={"logo.png"} alt="logo" /> 
               <AvatarFallback className='rounded-lg'>
                 SG
               </AvatarFallback>
               </Avatar>
           </div>
-          <span className='truncate text-2xl font-semibold'>SlideGenius</span>
+          <span className='truncate text-2xl font-normal text-slide-genius'>SlideGenius</span>
         </SidebarMenuButton>
       </SidebarHeader>
-      <SidebarContent className='px-3 mt-10 gap-y-6'>
+      <SidebarContent className='px-2 mt-10 gap-y-6'>
          <NavMain items={data.navMain}/> 
          <RecentOpen recentProjects={recentProjects} />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <NavFooter prismaUser={user}/>
+      </SidebarFooter>
     </Sidebar>
   )
 }
